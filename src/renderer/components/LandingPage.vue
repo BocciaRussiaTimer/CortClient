5d4131a1fe95192035c74bb264b791c9eb32fb93
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-08-14 23:05:47 +0300

    init project

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="wrapper">
+  
+    <header>
+      <h1>BocciaTimer</h1>
+    </header>
+    <main>
+      <match-form v-if="!match.init" :match.sync="match"></match-form>
+      <timer v-else></timer>
+    </main>
+  </div>
+</template>
+
+<script>
+import MatchForm from './LandingPage/MatchForm'
+import Timer from './LandingPage/Timer'
+
+export default {
+  data() {
+    return {
+      match: {
+        gameType:1,
+        init: false
+
+      }
+    }
+  },
+  components: { MatchForm, Timer },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link)
+    }
+  }
+}
+</script>
+
+<style>
+header {
+  position: absolute;
+  background: #734324;
+  color: #bcdcbc;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 5vh;
+}
+header>h1{
+  font-size: 3vh;
+  margin: 1vh;
+  
+}
+main{
+  position: absolute;
+  top: 6vh;
+  width: 100vw;
+}
+</style>
---
+++ src/renderer/components/LandingPage/MatchForm.vue
@@ -0,0 +1,38 @@
+<template> 
+<form @submit.prevent="submit">
+  <div class="form-group">
+     <label for="gameType">Тип игры</label>
+    <select class="form-control" id="gameType" v-model="match.gameType">
+      <option value="0">BC1</option>
+      <option value="1">BC2</option>
+      <option value="2">BC3</option>
+      <option value="3">BC4</option>
+      <option value="4">BC1 - BC2 teams</option>
+      <option value="5">BC3 pairs</option>
+      <option value="6">BC4 pairs</option>
+    </select>
+    </div>
+  <div class="form-group">
+    <label for="redname">Название команды красных</label>
+    <input class="form-control" id="redname" >
+  </div>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</template>
+
+<script>
+export default {
+  props:['match'],
+  methods:{
+      submit(){
+          
+      }
+  }
+}
+</script>
