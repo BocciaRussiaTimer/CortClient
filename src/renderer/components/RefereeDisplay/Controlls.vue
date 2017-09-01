6e405999b171d1926e55bbcec32fc299de15724a
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-09-01 21:45:50 +0300

    fix timer stop

--- src/renderer/components/RefereeDisplay/Controlls.vue
+++ src/renderer/components/RefereeDisplay/Controlls.vue
@@ -3,7 +3,6 @@
         <button class="btn" @click="match.end--">Предыдущий энд</button>
         <button class="btn" @click="timer('red')" :class="{'btn-success':match.players.red.ticking}">Запустить время красных</button>
         <button class="btn" @click="match.init=!match.init" :class="{'btn-success':!match.init}">Информация о игре</button>
-
         <button class="btn" @click="timer('blue')" :class="{'btn-success':match.players.blue.ticking}">Запустить время синих</button>
         <button class="btn" @click="match.end++">Следующий энд</button>
     </div>
@@ -16,9 +15,9 @@ import swalertcss from 'sweetalert/dist/sweetalert.css'
 
 export default {
     props: ['match'],
-    data () {
+    data() {
         return {
-            timerid:null,
+            timerid: null,
             player: null
         }
     },
@@ -26,16 +25,22 @@ export default {
         timer(side) {
             let player = this.match.players[side];
             this.stop();
+            if (this.player != null && this.player.side == player.side) {
+                this.player = null;
+                
+                return;
+            }
             this.player = player;
             player.ticking = true;
             this.timerid = setInterval(() => { this.tick() }, 1000);
             if (player.time[this.match.end] < 1) return this.timeover();
-            
+
 
         },
         tick() {
             Vue.set(this.player.time, this.match.end, this.player.time[this.match.end] - 1);
             if (this.player.time[this.match.end] < 1) {
+                this.player = null;
                 this.timeover();
             }
         },
@@ -45,10 +50,11 @@ export default {
             this.stop();
         },
         stop() {
+
             if (this.player != null) {
+                console.log('stop')
                 clearInterval(this.timerid);
                 this.player.ticking = false;
-                this.player = null;
             }
         }
     }
@@ -72,5 +78,6 @@ export default {
 .btn {
     height: 100%;
     width: 15vw;
+    color: black;
 }
 </style>
