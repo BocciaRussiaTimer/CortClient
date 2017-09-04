f1f17441cd44e6786a01cbd7de53b8f1ccb55478
Ivan Bakaidov <aacidov@Air-Ivan.lan>
2017-09-04 10:12:09 +0300

    add print button

--- src/renderer/components/RefereeDisplay/MatchForm.vue
+++ src/renderer/components/RefereeDisplay/MatchForm.vue
@@ -1,49 +1,55 @@
-<template> 
-<form @submit.prevent="submit">
-  <div class="form-group">
-     <label for="gameType">Тип игры</label>
-    <select class="form-control" id="gameType" v-model="match.gameType">
-      <option value=0>BC1</option>
-      <option value=1>BC2</option>
-      <option value=2>BC3</option>
-      <option value=3>BC4</option>
-      <option value=4>BC1 - BC2 команды</option>
-      <option value=5>BC3 пары</option>
-      <option value=6>BC4 пары</option>
-    </select>
+<template>
+  <form @submit.prevent="submit">
+    <div class="form-group">
+      <label for="gameType">Тип игры</label>
+      <select class="form-control" id="gameType" v-model="match.gameType">
+        <option value=0>BC1</option>
+        <option value=1>BC2</option>
+        <option value=2>BC3</option>
+        <option value=3>BC4</option>
+        <option value=4>BC1 - BC2 команды</option>
+        <option value=5>BC3 пары</option>
+        <option value=6>BC4 пары</option>
+      </select>
     </div>
-  <div class="form-group">
-    <label for="cort">Корт</label>
-    <input type="number" class="form-control" id="cort" v-model="match.cort" >
-  </div>
-  <div class="form-group">
-    <label for="redname">{{match.gameType>3?"Название команды, которая":"Имя игрока, который"}} играет красными</label>
-    <input class="form-control" id="redname" v-model="match.players.red.name" >
-  </div>
-  <div class="form-group">
-    <label for="bluename">{{match.gameType>3?"Название команды, которая":"Имя игрока, который"}} играет синими</label>
-  <input class="form-control" id="bluename" v-model="match.players.blue.name" >
-  </div>
+    <div class="form-group">
+      <label for="cort">Корт</label>
+      <input type="number" class="form-control" id="cort" v-model="match.cort">
+    </div>
+    <div class="form-group">
+      <label for="redname">{{match.gameType>3?"Название команды, которая":"Имя игрока, который"}} играет красными</label>
+      <input class="form-control" id="redname" v-model="match.players.red.name">
+    </div>
+    <div class="form-group">
+      <label for="bluename">{{match.gameType>3?"Название команды, которая":"Имя игрока, который"}} играет синими</label>
+      <input class="form-control" id="bluename" v-model="match.players.blue.name">
+    </div>
 
-  <div class="form-group">
-    <label for="referee">Имя Фамилия судьи</label>
-    <input  class="form-control" id="referee" v-model="match.referee" >
-  </div>
-  <div class="form-group">
-    <label for="referee">Имя Фамилия линейного судьи</label>
-    <input  class="form-control" id="referee" v-model="match.lineReferee" >
-  </div>
-  <button type="submit" class="btn btn-primary">Сохранить</button>
-</form>
+    <div class="form-group">
+      <label for="referee">Имя Фамилия судьи</label>
+      <input class="form-control" id="referee" v-model="match.referee">
+    </div>
+    <div class="form-group">
+      <label for="referee">Имя Фамилия линейного судьи</label>
+      <input class="form-control" id="referee" v-model="match.lineReferee">
+    </div>
+    <div class="form-group">
+    <printer/>
+    <button type="submit" class="btn btn-primary">Сохранить</button>
+    </div>
+  </form>
 </template>
 
 <script>
+import Printer from './Printer'
+
 export default {
-  props:['match'],
-  methods:{
-      submit(){
-          this.match.init=true;
-      }
+  props: ['match'],
+  components: { Printer },
+  methods: {
+    submit() {
+      this.match.init = true;
+    }
   }
 }
 </script>
---
+++ src/renderer/components/RefereeDisplay/Printer.vue
@@ -0,0 +1,28 @@
+<template>
+        <input type="button" class="btn btn-primary" @click="print" value="Отправить протокол на печать"></input>
+</template>
+
+<script>
+export default {
+    props: ['match'],
+    data(){
+        return {
+            protocol:'http',
+            domain:'127.0.0.1',
+            port: 30101
+        };
+    },
+    computed:{
+        url(){
+            return `${this.protocol}://${this.domain}:${this.port}/print`;
+        }
+    },
+    methods:{
+        print(){
+            this.$http.post(this.url, this.match).then((data)=>{
+                
+            })
+        }
+    }
+}
+</script>
